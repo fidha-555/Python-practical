f90804231d122d3562c71ac48bee18a06e9ee6f8
Fidha <fidhacherukad555@gmail.com>
2026-09-04 12:28:29 +0530

    file read example

--- Advanced functions/keyword_args.py
+++ Advanced functions/keyword_args.py
@@ -1,3 +1,5 @@
 def introduce(name,age):
     print(f"I'm {name} and I'm {age} years old")
 introduce(24,"Aadhi")
+
+
---
+++ File handling/file.py
@@ -0,0 +1,11 @@
+
+
+file = open ("C:\\Users\\HP\\python\\Advanced functions\\File handling\\notes.txt", "r")
+
+
+content = file.read()
+print(content)
+file.close()
+
+
+            
