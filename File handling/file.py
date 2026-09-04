d94f0e7c54632fd87a1c36bba6c61afc66e0202c
Fidha <fidhacherukad555@gmail.com>
2026-09-04 12:32:04 +0530

    file read line by line  example

--- File handling/file.py
+++ File handling/file.py
@@ -1,10 +1,8 @@
 
 
 file = open ("C:\\Users\\HP\\python\\Advanced functions\\File handling\\notes.txt", "r")
-
-
-content = file.read()
-print(content)
+for f in file:
+   print(f.strip())  
 file.close()
 
 
