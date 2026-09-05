17198f783945d64d74ebad619e9a95c0dd5ac297
Fidha <fidhacherukad555@gmail.com>
2026-09-05 21:50:25 +0530

    even & odd test  example

---
+++ module/my_package/test.py
@@ -0,0 +1,5 @@
+import utility2
+
+num = int(input("Enter the number:"))
+print("Even number",utility2.is_even(num))
+print("Odd number",utility2.is_odd(num))
---
+++ module/my_package/utility2.py
@@ -0,0 +1,5 @@
+def is_even(num):
+   return num % 2 ==0
+
+def is_odd(num):
+   return num % 2 != 0
