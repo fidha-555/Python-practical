b7144631c2e3d6b1955f1cbea1567ca69f14a1cd
Fidha <fidhacherukad555@gmail.com>
2026-09-07 14:59:46 +0530

    hierarchical  inheritance  example

---
+++ Advanced functions/OOPS/hierarchical_inheritance.py
@@ -0,0 +1,14 @@
+class Vehicle:
+    def start(self):
+        print("Vehicle starting..")
+class Car(Vehicle):
+    pass
+
+class Bike(Vehicle):
+    pass
+
+c= Car()
+c.start()
+
+b= Bike()
+b.start()
